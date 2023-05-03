1d4f0828abc7458f88a8a15dcd3aab8bf4bbdeaf
gaju rosine <gajurossine1@gmail.com>
2023-05-03 11:28:17 +0200

    working on aim page putting  new  features

--- src/Component/Pages/Aim.js
+++ src/Component/Pages/Aim.js
@@ -1,24 +1,45 @@
 import React from 'react';
-
+import code from  '../../Asset/images/loxa.png'
+import { AiTwotoneMail } from 'react-icons/ai';
+import { FiMapPin ,FiFile , FiUser } from "react-icons/fi";
 function AIM(props) {
     return (
         <div className='contents'>
               <div className='improduction'>
                 <div className='dev_greet'>
-                <h1 className='aim_greeting'>Hello world , I'm  Rosine</h1>
-                <p className='aim_para'>am a  web  developer  and  web designer who  is  a students</p>
+                <div className='user'> <FiUser  className='fiuser-icon'/></div>
+                <h2 className='title' >Hi , I'm <span style={{color : '#e71414', fontSize:'40px' }} >Rosine</span></h2>
+                <p  style={{color:'#ffffff' ,fontSize:'20px' }}> Web design and  web developer  working at  my company</p>
+                <div className='user-info'>
+
+                <div className='info'>
+                  <FiFile className='info-icon'/>
+                  <span className='icon-desc' >Web design and developer </span>
+                </div>
+             
+                <div className='info'>
+                 <AiTwotoneMail  className='info-icon' />
+                  <span className='icon-desc'>gajurosine1@gmail.com</span>
+                </div>
+             
+                <div className='info'>
+                <FiMapPin   className='info-icon'/>
+                  <span  className='icon-desc'>Rwand , kigali city</span>
+                </div>
+             
+
+                </div>
+                
               </div>
 
               <div className='downlods-butto'>
-                <p className='downloads'>Download my curriculum vitae:</p>
-                <button className='cv'>DownloadsCV</button>
+                <button className='contactme'>DownloadsCV</button>
                 <button className='contactme'>Contact</button>
               </div>
               </div>
                  
                 <div className='pic_me'>
-                    <h1>pic</h1>
-               
+                 <img  src={code} alt='codes' className='aim-imag'></img>               
               </div>
 
         </div>
