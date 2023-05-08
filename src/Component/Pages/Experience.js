22e43bf2643f842fd9f31b6dae74f9c714dc66bb
gaju rosine <gajurosine1@gmail.com>
2023-05-08 16:43:57 +0200

    all  of  my  experience  page

--- src/Component/Pages/Experience.js
+++ src/Component/Pages/Experience.js
@@ -2,24 +2,63 @@ import React from 'react';
 import codes from '../../Asset/images/code.avif'
 function Experience(props) {
     return (
+        
         <div className='myExperience'>
      <div className='EX-content'>
-    <h1>OVER 3 YEARS OF EXPERIENCE</h1>
-    <p>My Experience</p>
+    <h1 className='years'>OVER 3 YEARS OF EXPERIENCE</h1>
+    <p className='para-exp'>My Experience</p>
          </div>
        <div className='my-experience'>
-        <div className='develop'>
-            <div>
-            <img src={codes} alt='devolop experience' />
-            </div>
+        <div className='develop-img'>
+        <img src={codes} alt='devolop experience' className='img_devops' />
+        </div>
             <div className='exp-desc'>
                 <h2 className='tit-exp'>2023-Presents</h2>
                 <p className='exp-develop'>Software  developer</p>
+                <p className='exp-develop'>Web desing</p>
             </div>
             <div className='exp-butto'><button>CONTACT</button></div>
-        </div>
        </div>
+
+
+
+       <div className='my-experience'>
+        <div className='develop-img'>
+        <img src={codes} alt='devolop experience' className='img_devops' />
         </div>
+            <div className='exp-desc'>
+                <h2 className='tit-exp'>2023-Presents</h2>
+                <p className='exp-develop'>Data science</p>
+                <p className='exp-develop'>Web desing</p>
+            </div>
+            <div className='exp-butto'><button>CONTACT</button></div>
+       </div>
+
+
+
+
+
+       <div className='my-experience'>
+        <div className='develop-img'>
+        <img src={codes} alt='devolop experience' className='img_devops' />
+        </div>
+            <div className='exp-desc'>
+                <h2 className='tit-exp'>2023-Presents</h2>
+                <p className='exp-develop'>Robotics design</p>
+                <p className='exp-develop'>Web desing</p>
+            </div>
+            <div className='exp-butto'><button>CONTACT</button></div>
+       </div>
+       
+        </div>
+
+
+
+     
+
+
+
+
     );
 }
 
